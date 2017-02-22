6d52ffd1da61824c128b97fa1d672fbec0994280
sakai <sakai.417.tblbj@gmail.com>
2017-02-22 17:40:35 +0900

    commentの前まで

--- app/controllers/my_threads_controller.rb
+++ app/controllers/my_threads_controller.rb
@@ -1,2 +1,47 @@
 class MyThreadsController < ApplicationController
-end
+
+    before_action :set_my_thread, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @my_threads = MyThread.all
+    end
+
+    def new
+      @my_thread = MyThread.new
+    end
+
+    def create
+      @my_thread = MyThread.new(my_thread_params)
+      # saveされてたらredirectでindexにいく
+      if @my_thread.save
+        redirect_to my_threads_path
+        # validateで弾かれてsaveに失敗したらnewにリダイレクト
+      else
+        render 'new'
+      end
+    end
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      @my_thread.update(my_thread_params)
+      redirect_to my_thread_path(@my_thread)
+    end
+
+    def destroy
+      @my_thread.destroy
+      redirect_to my_threads_url(@my_thread)
+    end
+
+    private
+    def my_thread_params
+      params[:my_thread].permit(:title, :overview)
+    end
+
+    def set_my_thread
+      @my_thread = MyThread.find(params[:id])
+    end
+  end
--- db/migrate/20170222082722_create_my_threads.rb
+++ db/migrate/20170222082722_create_my_threads.rb
@@ -2,7 +2,7 @@ class CreateMyThreads < ActiveRecord::Migration[5.0]
   def change
     create_table :my_threads do |t|
       t.string :title
-      t.overview :content
+      t.string :overview
 
       t.timestamps
     end
