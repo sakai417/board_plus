c713a8ca770e6b9da4e188555eb84106d7623dce
sakai <sakai.417.tblbj@gmail.com>
2017-02-25 15:09:17 +0900

    ログアウト後の処理まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :my_threads do
     resources :comments, only: [:index, :new, :create]
